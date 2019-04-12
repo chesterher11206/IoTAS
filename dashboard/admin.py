3f3915dd77a64b68960010e7ea0ae99784061f9f
chesterher11206 <chester11206@gmail.com>
2019-04-13 02:16:40 +0800

    IoT Website mqtt

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import ServiceInfo
+from .models import DeviceInfo
 
-admin.site.register(ServiceInfo)
+admin.site.register(DeviceInfo)
 
 # Register your models here.
